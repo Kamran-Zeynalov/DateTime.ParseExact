b8161ead071832fb1a92d24aeb9e506e5eb7823d
Kamran Z <kamraneze@code.edu.az>
2023-08-25 11:16:24 +0400

    I checked DateTime.ParseExact() with try/catch

---
+++ Birthday/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Birthday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        here:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Enter your birtday");
+            string? userBithday = Console.ReadLine();
+            try
+            {
+                var insert = DateTime.ParseExact(userBithday, "dd | MM | yyyy", null);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                goto here;
+            }
+
+
+        }
+    }
+}
